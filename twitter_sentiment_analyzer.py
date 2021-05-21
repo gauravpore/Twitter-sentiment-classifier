5d426c44172b808fb1bf6af8e17656ea51d273af
Gaurav Pore <poregaurav13@gmail.com>
2021-05-21 21:38:54 +0530

    source code

---
+++ twitter_sentiment_analyzer.py
@@ -0,0 +1,109 @@
+# importing neccessary libraries
+from textblob import TextBlob
+import sys
+import tweepy
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import nltk
+import pycountry
+import re
+import string
+
+# downloads
+import nltk
+
+nltk.download('vader_lexicon')
+
+# importing neccesary libraries
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from langdetect import detect
+from nltk.stem import SnowballStemmer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Authentication twitter API
+consumerKey = "h9jkfHBKsoDYSBQVEbN88hwUD"
+consumerSecret = "OaCXHWd9bLESKKYwQ8zdM0QUxw3XpSAtBOJBNyH3MlrFzyKE0v"
+accessToken = "2998005176-18xGkcZYv6wDq7dppD1KCNnjT9hux2it86H29Rn"
+accessTokenSecret = "ieiK9puuKugSLUgeuUqUVktzWVR7nfwh8ovOHRgroaz8d"
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+api = tweepy.API(auth)  # api authentication (twitter app)
+
+
+# Sentiment Analysis
+def percentage(part, whole):
+    return 100 * float(part) / float(whole)
+
+
+keyword = input("Please enter keyword or hashtag to search: ")
+noOfTweet = int(input("Please enter how many tweets to analyze: "))
+
+tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
+positive = 0
+negative = 0
+neutral = 0
+polarity = 0
+tweet_list = []
+neutral_list = []
+negative_list = []
+positive_list = []
+
+for tweet in tweets:
+
+    # print(tweet.text)
+    tweet_list.append(tweet.text)
+    analysis = TextBlob(tweet.text)
+    score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
+    neg = score['neg']
+    neu = score['neu']
+    pos = score['pos']
+    comp = score['compound']
+    polarity += analysis.sentiment.polarity
+
+    if neg > pos:
+        negative_list.append(tweet.text)
+        negative += 1
+    elif pos > neg:
+        positive_list.append(tweet.text)
+        positive += 1
+
+    elif pos == neg:
+        neutral_list.append(tweet.text)
+        neutral += 1
+
+positive = percentage(positive, noOfTweet)
+negative = percentage(negative, noOfTweet)
+neutral = percentage(neutral, noOfTweet)
+polarity = percentage(polarity, noOfTweet)
+positive = format(positive, ".1f")
+negative = format(negative, '.1f')
+neutral = format(neutral, '.1f')
+
+# Number of Tweets (Total, Positive, Negative, Neutral)
+tweet_list = pd.DataFrame(tweet_list)
+neutral_list = pd.DataFrame(neutral_list)
+negative_list = pd.DataFrame(negative_list)
+positive_list = pd.DataFrame(positive_list)
+print("total number: ", len(tweet_list))
+print("positive number: ", len(positive_list))
+print("negative number: ", len(negative_list))
+print("neutral number: ", len(neutral_list))
+
+# print (negative_list)
+print(tweet_list)
+
+# Creating PieCart for data visualization
+labels = ['Positive: ' + str(positive) + "%", 'Negative: ' + str(negative) + "%", 'Neutral: ' + str(neutral) + "%"]
+sizes = [positive, neutral, negative]
+colors = ['yellowgreen', 'blue', 'red']
+patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+plt.style.use('default')
+plt.legend(labels)
+plt.title("Sentiment Analysis Result for keyword= " + keyword)
+plt.axis('equal')
+plt.show()
